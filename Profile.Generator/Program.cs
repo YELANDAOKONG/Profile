06126b862ccb47e8f3d04783dcf5f8035758610e
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2026-07-28 12:14:46 +0800

    init: create project

---
+++ Profile.Generator/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Profile.Generator;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Profile.Worker/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Profile.Worker;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
